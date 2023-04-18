42fd8716096a81a2fa073b48c29f1f076f98ecfa
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-04-18 13:50:35 +0530

    Conn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require ("express");
 
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("Database Connected 🔥"))
+
 const bb = express()
 
 bb.use(express.json())
+
+bb.listen(3001,()=>console.log("Server is running 🚀"))
