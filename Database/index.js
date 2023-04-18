55a1e1313874f9e99a0bf3be8dbfae941c489ad1
Naveen Kumar <karanamunaveen2002@gmail.com>
2023-04-18 12:08:38 +0530

    V1

---
+++ Database/index.js
@@ -0,0 +1,44 @@
+const movies = [
+    {
+      id: "1",
+      title: "RRR",
+      directors: [1],
+      producers: [1],
+      language: "multi",
+      release_date: "25-03-2022",
+      generes: ["Action", "Drama", "Adventure"],
+    },
+    {
+        id: "2",
+        title: "BB",
+        directors: [1],
+        producers: [1],
+        language: "eng",
+        release_date: "25-03-2022",
+        generes: ["Action", "Drama", "Adventure"],
+      },
+  ];
+  
+  const directors = [
+    {
+      id: 1,
+      name: "Rajamouli",
+      movies: ["1"],
+    },
+  ];
+  
+  const producers = [
+    {
+      id: 1,
+      name: "D. V. V. Danayya",
+      movies: ["1"],
+    },
+  ];
+  
+  module.exports = { movies, directors, producers };
+
+const moviesss=[
+    {
+
+    }
+]
---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require ("express");
+
+const bb = express()
+
+bb.use(express.json())
